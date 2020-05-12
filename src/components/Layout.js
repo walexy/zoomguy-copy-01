9707cea9050807f758f1c9125f515873273ce673
Bill Alexy <bill@alexy19.com>
2020-05-12 16:51:22 -0500

    Update Layout.js

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -6,7 +6,6 @@ import {safePrefix} from '../utils';
 
 import Header from './Header';
 import Footer from './Footer';
-// {_.get(this.props, 'pageContext.frontmatter.ogimage')}
 
 export default class Body extends React.Component {
     render() {
@@ -23,7 +22,7 @@ export default class Body extends React.Component {
                     <meta property='og:type' content='article' />
                     <meta property='og:title' content={_.get(this.props, 'pageContext.frontmatter.title') && _.get(this.props, 'pageContext.frontmatter.title') + ' - ' + _.get(this.props, 'pageContext.site.siteMetadata.title')} />
                     <meta property='og:description' content={_.get(this.props, 'pageContext.frontmatter.subtitle') || _.get(this.props, 'pageContext.site.siteMetadata.description')} />
-                    <meta property='og:url' content={_.get(this.props, 'pageContext.site.siteMetadata.url')} />
+                    {/* <meta property='og:url' content={_.get(this.props, 'pageContext.frontmatter.subtitle') || _.get(this.props, 'pageContext.site.siteMetadata.url')} /> */}
                     <meta property='og:updated_time' content='2019-04-31' />
                     <link href="https://fonts.googleapis.com/css?family=Nunito+Sans:400,400i,700,700i" rel="stylesheet"/>
                     <link rel="stylesheet" href={safePrefix('assets/css/main.css')}/>
